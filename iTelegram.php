18edaa05f73e991968e21d8b09473f9a046ceb99
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2021-10-12 17:11:55 +0330

    iTelegram Class PHP File

--- iTelegram.php
+++ iTelegram.php
@@ -47,6 +47,7 @@ class Bot{
 			}
 		}
 	}
+	function TelegramAPI($method, $data = []){ return iNeoTeamBot($method, $data); }
 	function editMessage($chatID, $messageID, $text, $mode = null, $webPage = null, $button = null){
 		if($mode == "" or !in_array(strtolower($mode), ['markdown', 'html'])){
 			$mode = "html";
